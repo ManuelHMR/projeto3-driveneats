8d81bd1090d69107079307e39d8a3fab7a44db3b
ManuelHMR <manuelhmrjf@gmail.com>
2022-01-31 11:56:54 -0300

    basico do JS pronto

--- scripts.js
+++ scripts.js
@@ -1,5 +1,12 @@
-let nameArray = []
-let priceArray = []
+let dishesName = ''
+let drinksName = ''
+let dessertsName = ''
+let dishesPrice = ''
+let drinksPrice = ''
+let dessertsPrice = ''
+let fullPrice = 0
+let adress = ''
+let userName = ''
 function selectItem(el) {
   let selectedElement = el.parentNode.querySelector('.select')
   selectedElement?.classList.remove('select')
@@ -19,7 +26,31 @@ function allSelected() {
     h1.innerHTML = 'Selecione os 3 itens para fechar o pedido'
   }
 }
+function orderResume(){
+  let selectArray = document.querySelectorAll('.select')
+   dishesName = selectArray[0].querySelector('h1').innerHTML
+   drinksName = selectArray[1].querySelector('h1').innerHTML
+   dessertsName = selectArray[2].querySelector('h1').innerHTML
+   dishesPrice = parseFloat(selectArray[0].querySelector('h3').innerHTML.replace(',','.').replace('R$', ""))
+   drinksPrice = parseFloat(selectArray[1].querySelector('h3').innerHTML.replace(',','.').replace('R$', ""))
+   dessertsPrice = parseFloat(selectArray[2].querySelector('h3').innerHTML.replace(',','.').replace('R$ ', ""))
+}
+function orderPrice(){
+  fullPrice = (dishesPrice + drinksPrice + dessertsPrice).toFixed(2).toString().replace('.',',')
+}
 function finishOrder() {
-  document.querySelector('.finish_back_ground').classList.add('enabled')
-  alert(dishesSelected)
+  //document.querySelector('.finish_back_ground').classList.add('enabled')
+  orderResume()
+  orderPrice()
+  userName = prompt('Qual o seu nome?')
+  adress = prompt('Qual o seu endereço?')
+  const final_message = `Olá, gostaria de fazer o pedido:
+  - Prato: ${dishesName}
+  - Bebida: ${drinksName}
+  - Sobremesa: ${dessertsName}
+  Total: R$ ${fullPrice}
+  
+  Nome: ${userName}
+  Endereço: ${adress}`
+  window.open(`https://wa.me/553284161596?text=${encodeURIComponent(final_message)}`);
 }
