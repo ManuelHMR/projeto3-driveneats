cb101566a3d0dbc0acbf90eee5cf89fe0d3363ba
ManuelHMR <manuelhmrjf@gmail.com>
2022-01-30 23:56:09 -0300

    preparando resumo do pedido

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+let nameArray = []
+let priceArray = []
 function selectItem(el) {
   let selectedElement = el.parentNode.querySelector('.select')
   selectedElement?.classList.remove('select')
@@ -10,10 +12,14 @@ function allSelected() {
   let footerButton = document.querySelector('.footer_button')
   let h1 = footerButton.querySelector('h1')
   if (document.querySelectorAll('.select').length === 3) {
-    footerButton.classList.add('enabled')
+    footerButton.disabled = false
     h1.innerHTML = 'Fechar pedido'
   } else {
-    footerButton.classList.remove('enabled')
+    footerButton.disabled = true
     h1.innerHTML = 'Selecione os 3 itens para fechar o pedido'
   }
+}
+function finishOrder() {
+  document.querySelector('.finish_back_ground').classList.add('enabled')
+  alert(dishesSelected)
 }
